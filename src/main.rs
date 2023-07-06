f58f2c22e6f5a6f13d8ddb370b5823ff8e44e54f
Masterchef365 <duncan.freeman1@gmail.com>
2023-07-05 23:34:29 -0700

    Add triangle painting

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use eframe::egui::{self, Context, DragValue, SidePanel, Ui};
+use egui::mutex::Mutex;
+use std::sync::Arc;
 
 #[derive(PartialEq)]
 enum Tabs {
     Record,
     Calibrate,
+}
+
+struct MyApp {
+    pattern: Arc<Mutex<ProjectorPatternPainter>>,
+    cfg: AppConfig,
 }
 
 #[derive(Default)]
@@ -24,14 +31,18 @@ struct RecorderConfig {
     pics_per_pattern: usize,
 }
 
-fn main() -> eframe::Result<()> {
-    let mut cfg = AppConfig::default();
-
-    let options = eframe::NativeOptions::default();
-    eframe::run_simple_native("Deproject calibrator", options, move |ctx, frame| {
-        frame.set_fullscreen(true);
-        SidePanel::left("Left").show(ctx, |ui| app_ui(ui, &mut cfg));
-    })
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(350.0, 380.0)),
+        multisampling: 4,
+        renderer: eframe::Renderer::Glow,
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Custom 3D painting in eframe using glow",
+        options,
+        Box::new(|cc| Box::new(MyApp::new(cc))),
+    )
 }
 
 fn app_ui(ui: &mut Ui, state: &mut AppConfig) {
@@ -52,7 +63,7 @@ fn app_ui(ui: &mut Ui, state: &mut AppConfig) {
 fn record_ui(ui: &mut Ui, state: &mut RecorderConfig) {
     // Subdivision
     ui.strong("Subdivisions");
-    ui.label("Controls the granularity of the calibration pattern displayed by the projector, in powers of 2. Optimally, this should be the resolution of the projector.");
+    ui.label("Controls the granularity of the calibration pattern displayed by the projector, in powers of 2. This should be close to the resolution of the projector.");
     ui.add(
         DragValue::new(&mut state.horiz_subdivs)
             .prefix("Horizontal resolution: ")
@@ -111,8 +122,8 @@ impl Default for CalibratorConfig {
 impl Default for RecorderConfig {
     fn default() -> Self {
         Self {
-            horiz_subdivs: 12,
-            vert_subdivs: 11,
+            horiz_subdivs: 11,
+            vert_subdivs: 10,
             pics_per_pattern: 1,
         }
     }
@@ -121,5 +132,171 @@ impl Default for RecorderConfig {
 impl Default for Tabs {
     fn default() -> Self {
         Self::Record
+    }
+}
+
+
+impl MyApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        let gl = cc
+            .gl
+            .as_ref()
+            .expect("You need to run eframe with the glow backend");
+        Self {
+            pattern: Arc::new(Mutex::new(ProjectorPatternPainter::new(gl))),
+            cfg: AppConfig::default(),
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::SidePanel::left("Left").show(ctx, |ui| {
+            app_ui(ui, &mut self.cfg);
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            self.custom_painting(ui);
+        });
+    }
+
+    fn on_exit(&mut self, gl: Option<&glow::Context>) {
+        if let Some(gl) = gl {
+            self.pattern.lock().destroy(gl);
+        }
+    }
+}
+
+impl MyApp {
+    fn custom_painting(&mut self, ui: &mut egui::Ui) {
+        let (rect, _response) =
+            ui.allocate_exact_size(ui.available_size(), egui::Sense::drag());
+
+        // Clone locals so we can move them into the paint callback:
+        let pattern = self.pattern.clone();
+
+        let callback = egui::PaintCallback {
+            rect,
+            callback: std::sync::Arc::new(egui_glow::CallbackFn::new(move |_info, painter| {
+                pattern.lock().paint(painter.gl());
+            })),
+        };
+        ui.painter().add(callback);
+    }
+}
+
+struct ProjectorPatternPainter {
+    program: glow::Program,
+    vertex_array: glow::VertexArray,
+}
+
+impl ProjectorPatternPainter {
+    fn new(gl: &glow::Context) -> Self {
+        use glow::HasContext as _;
+
+        let shader_version = if cfg!(target_arch = "wasm32") {
+            "#version 300 es"
+        } else {
+            "#version 330"
+        };
+
+        unsafe {
+            let program = gl.create_program().expect("Cannot create program");
+
+            let (vertex_shader_source, fragment_shader_source) = (
+                r#"
+                    const vec2 verts[3] = vec2[3](
+                        vec2(0.0, 1.0),
+                        vec2(-1.0, -1.0),
+                        vec2(1.0, -1.0)
+                    );
+                    const vec4 colors[3] = vec4[3](
+                        vec4(1.0, 0.0, 0.0, 1.0),
+                        vec4(0.0, 1.0, 0.0, 1.0),
+                        vec4(0.0, 0.0, 1.0, 1.0)
+                    );
+                    out vec4 v_color;
+                    uniform float u_angle;
+                    void main() {
+                        v_color = colors[gl_VertexID];
+                        gl_Position = vec4(verts[gl_VertexID], 0.0, 1.0);
+                        gl_Position.x *= cos(u_angle);
+                    }
+                "#,
+                r#"
+                    precision mediump float;
+                    in vec4 v_color;
+                    out vec4 out_color;
+                    void main() {
+                        out_color = v_color;
+                    }
+                "#,
+            );
+
+            let shader_sources = [
+                (glow::VERTEX_SHADER, vertex_shader_source),
+                (glow::FRAGMENT_SHADER, fragment_shader_source),
+            ];
+
+            let shaders: Vec<_> = shader_sources
+                .iter()
+                .map(|(shader_type, shader_source)| {
+                    let shader = gl
+                        .create_shader(*shader_type)
+                        .expect("Cannot create shader");
+                    gl.shader_source(shader, &format!("{}\n{}", shader_version, shader_source));
+                    gl.compile_shader(shader);
+                    assert!(
+                        gl.get_shader_compile_status(shader),
+                        "Failed to compile {shader_type}: {}",
+                        gl.get_shader_info_log(shader)
+                    );
+                    gl.attach_shader(program, shader);
+                    shader
+                })
+                .collect();
+
+            gl.link_program(program);
+            assert!(
+                gl.get_program_link_status(program),
+                "{}",
+                gl.get_program_info_log(program)
+            );
+
+            for shader in shaders {
+                gl.detach_shader(program, shader);
+                gl.delete_shader(shader);
+            }
+
+            let vertex_array = gl
+                .create_vertex_array()
+                .expect("Cannot create vertex array");
+
+            Self {
+                program,
+                vertex_array,
+            }
+        }
+    }
+
+    fn destroy(&self, gl: &glow::Context) {
+        use glow::HasContext as _;
+        unsafe {
+            gl.delete_program(self.program);
+            gl.delete_vertex_array(self.vertex_array);
+        }
+    }
+
+    fn paint(&self, gl: &glow::Context) {
+        use glow::HasContext as _;
+        unsafe {
+            gl.use_program(Some(self.program));
+            gl.uniform_1_f32(
+                gl.get_uniform_location(self.program, "u_angle").as_ref(),
+                0.,
+            );
+            gl.bind_vertex_array(Some(self.vertex_array));
+            gl.draw_arrays(glow::TRIANGLES, 0, 3);
+        }
     }
 }
