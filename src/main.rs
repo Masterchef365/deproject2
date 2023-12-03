95e35661a641f5aec5dec7af34ae99f0d63d6045
Duncan Freeman <duncan.freeman1@gmail.com>
2023-12-02 16:20:12 -0800

    DESTROY BUILD DESTROY

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,7 @@ enum Tabs {
     Calibrate,
 }
 
-type PointCloud = Vec<Vertex>;
-
 struct MyApp {
-    pattern: Arc<Mutex<ProjectorPatternPainter>>,
     view3d: Arc<Mutex<Viewport3d>>,
     viewport_state: ViewportState,
     cfg: AppConfig,
@@ -172,7 +169,6 @@ impl MyApp {
             viewport_state: ViewportState::default(),
             view3d: Arc::new(Mutex::new(view3d)),
             render_tx,
-            pattern: Arc::new(Mutex::new(ProjectorPatternPainter::new(gl))),
             cfg: AppConfig::default(),
         }
     }
@@ -193,135 +189,8 @@ impl eframe::App for MyApp {
     }
 
     fn on_exit(&mut self, gl: Option<&glow::Context>) {
-        if let Some(gl) = gl {
-            self.pattern.lock().destroy(gl);
-        }
-    }
-}
-
-impl MyApp {
-    fn show_calibration_pattern(&mut self, ui: &mut egui::Ui) {
-        let (rect, _response) = ui.allocate_exact_size(ui.available_size(), egui::Sense::drag());
-
-        // Clone locals so we can move them into the paint callback:
-        let pattern = self.pattern.clone();
-
-        let window_size = window_size_in_pixels(ui.ctx());
-
-        if false {
-            let callback = egui::PaintCallback {
-                rect,
-                callback: std::sync::Arc::new(egui_glow::CallbackFn::new(move |_info, painter| {
-                    pattern.lock().paint(painter.gl(), window_size);
-                })),
-            };
-            ui.painter().add(callback);
-        }
-    }
-}
-
-struct ProjectorPatternPainter {
-    program: glow::Program,
-    vertex_array: glow::VertexArray,
-}
-
-impl ProjectorPatternPainter {
-    fn new(gl: &glow::Context) -> Self {
-        use glow::HasContext as _;
-
-        let shader_version = if cfg!(target_arch = "wasm32") {
-            "#version 300 es"
-        } else {
-            "#version 330"
-        };
-
-        unsafe {
-            let program = gl.create_program().expect("Cannot create program");
-
-            let (vertex_shader_source, fragment_shader_source) = (
-                r#"
-                    // https://www.saschawillems.de/blog/2016/08/13/vulkan-tutorial-on-rendering-a-fullscreen-quad-without-buffers/
-                    out vec2 uv;
-
-                    void main() {
-                        uv = vec2((gl_VertexID << 1) & 2, gl_VertexID & 2);
-                        gl_Position = vec4(uv * 2.0f + -1.0f, 0.0f, 1.0f);
-                    }
-                "#,
-                r#"
-                    precision mediump float;
-                    in vec2 uv;
-                    out vec4 out_color;
-                    void main() {
-                        out_color = vec4(uv, 0, 1);
-                    }
-                "#,
-            );
-
-            let shader_sources = [
-                (glow::VERTEX_SHADER, vertex_shader_source),
-                (glow::FRAGMENT_SHADER, fragment_shader_source),
-            ];
-
-            let shaders: Vec<_> = shader_sources
-                .iter()
-                .map(|(shader_type, shader_source)| {
-                    let shader = gl
-                        .create_shader(*shader_type)
-                        .expect("Cannot create shader");
-                    gl.shader_source(shader, &format!("{}\n{}", shader_version, shader_source));
-                    gl.compile_shader(shader);
-                    assert!(
-                        gl.get_shader_compile_status(shader),
-                        "Failed to compile {shader_type}: {}",
-                        gl.get_shader_info_log(shader)
-                    );
-                    gl.attach_shader(program, shader);
-                    shader
-                })
-                .collect();
-
-            gl.link_program(program);
-            assert!(
-                gl.get_program_link_status(program),
-                "{}",
-                gl.get_program_info_log(program)
-            );
-
-            for shader in shaders {
-                gl.detach_shader(program, shader);
-                gl.delete_shader(shader);
-            }
-
-            let vertex_array = gl
-                .create_vertex_array()
-                .expect("Cannot create vertex array");
-
-            Self {
-                program,
-                vertex_array,
-            }
-        }
-    }
-
-    fn destroy(&self, gl: &glow::Context) {
-        use glow::HasContext as _;
-        unsafe {
-            gl.delete_program(self.program);
-            gl.delete_vertex_array(self.vertex_array);
-        }
-    }
-
-    fn paint(&self, gl: &glow::Context, size: Vec2) {
-        use glow::HasContext as _;
-        unsafe {
-            // Take up the whole screen!
-            gl.scissor(0, 0, size.x as _, size.y as _);
-            gl.viewport(0, 0, size.x as _, size.y as _);
-
-            gl.use_program(Some(self.program));
-            gl.bind_vertex_array(Some(self.vertex_array));
-            gl.draw_arrays(glow::TRIANGLES, 0, 3);
+        if let Some(_gl) = gl {
+            // TODO: Destroy viewport3d here?
         }
     }
 }
